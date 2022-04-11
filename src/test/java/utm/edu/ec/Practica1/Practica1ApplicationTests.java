dbd60283ab6192c196a9023e1fd9e29e51bcd382
Walter <39349874+wparraga@users.noreply.github.com>
2022-04-11 15:23:18 -0500

    initial commit

---
+++ src/test/java/utm/edu/ec/Practica1/Practica1ApplicationTests.java
@@ -0,0 +1,13 @@
+package utm.edu.ec.Practica1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
